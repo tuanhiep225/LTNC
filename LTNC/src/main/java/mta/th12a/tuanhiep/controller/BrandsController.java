45affba1a4762f5409f38ddee2b41e1b1a1e329f
tuanhiep225 <tuanhiep225@gmail.com>
2016-10-22 05:37:27 +0700

    Modelandview

--- LTNC/src/main/java/mta/th12a/tuanhiep/controller/BrandsController.java
+++ LTNC/src/main/java/mta/th12a/tuanhiep/controller/BrandsController.java
@@ -1,15 +1,22 @@
 package mta.th12a.tuanhiep.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import mta.th12a.tuanhiep.service.IBrandsService;
 
 @Controller
 @RequestMapping(value="/brand")
 public class BrandsController {
+	@Autowired
+	IBrandsService brandService;
 	@RequestMapping(value="/index")
-	public String Index()
+	public ModelAndView Index()
 	{
-		return "index";
+		
+		return new ModelAndView("index","listBrand",brandService.getAll());
 	}
 }
