7577177656684e048b672212018b414c9859ae4a
tuanhiep225 <tuanhiep225@gmail.com>
2016-10-23 17:12:59 +0700

    Minh

--- LTNC/src/main/java/mta/th12a/tuanhiep/controller/CategoriesController.java
+++ LTNC/src/main/java/mta/th12a/tuanhiep/controller/CategoriesController.java
@@ -1,8 +1,10 @@
 package mta.th12a.tuanhiep.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/category")
 public class CategoriesController {
 
 }
